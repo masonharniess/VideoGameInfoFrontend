cf5594507650e949744da9d95abab01f6118ad27
Mason Harniess <maseharniess@outlook.com>
2023-08-17 17:27:12 +0100

    clean navigation bar and add style

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from "@angular/core";
 
 @Component({
-    templateUrl: './home.component.html'
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-    public fileName = 'home.component.html';
+    public pageTitle = 'VideoGameInfo';
 }
